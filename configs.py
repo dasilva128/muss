fb88305e9ec2f5e39d4fb027e28c944234363288
dasilva128 <hasoonhandavi@gmail.com>
2025-07-17 16:12:10 +0330

    Upload via bot

---
+++ configs.py
@@ -0,0 +1,4 @@
+# config.py
+BOT_TOKEN = "YOUR_BOT_TOKEN"  # توکن ربات خود را از BotFather جایگذاری کنید
+MUSIC_DIRECTORY = "./music_files/"  # مسیر ذخیره فایل‌های موسیقی
+ALLOWED_GROUP_ID = -100123456789  # آیدی گروه تلگرام (اختیاری برای محدود کردن دسترسی)
